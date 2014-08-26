eb3a4bf4217db31f28223e6f667e411e04d21350
shashmi <shashmi@klipfolio.com>
2014-08-26 09:07:58 -0400

    remove printing

--- MetricsApp.py
+++ MetricsApp.py
@@ -217,8 +217,7 @@ def main():
     global dir_name, main_file, main_writer    
     if companyIDs:
         ids = companyIDs.split(',')
-        ids = [i.strip() for i in ids]
-        print ids
+        ids = [i.strip() for i in ids]        
         query = "SELECT id, public_id, name FROM company c WHERE c.public_id in ({})".format(', '.join('"{0}"'.format(id) for id in  ids))   
     if partner and not companyIDs:
         cursor.execute("SELECT name FROM company WHERE public_id='{}'".format(partner))        
